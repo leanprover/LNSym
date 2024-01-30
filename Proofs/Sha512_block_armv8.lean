0babc6a19ffa0ce89527b23f7cf27153523571db
Shilpi Goel <shigoel@gmail.com>
2024-01-29 18:23:25 -0600

    Document simp/ground issues in SHA512 symbolic simulation attempts

--- Proofs/Sha512_block_armv8.lean
+++ Proofs/Sha512_block_armv8.lean
@@ -10,41 +10,36 @@ section SHA512_proof
 
 open Std.BitVec
 
+/-
+The `sym1` tactic, defined in ../Tactics/Sym.lean, attempts to
+symbolically simulate a single instruction -- it is pretty
+straightforward right now and we expect it to radically change in the
+near future.
+
+It unfolds the `run` function once, then
+unfolds the `stepi` function, reduces
+`fetch_inst <address> <state>`
+to a term like:
+`Std.RBMap.find?  <program> <address>`
+where `<program>` and `<address>` are expected to be concrete.
+We hope that `simp` is able to "evaluate" such ground terms during
+proof so that the above gives us instruction <i> located at
+`<address>` in `<program>`. Moreover, we also hope that `simp`
+evaluates `decode_raw_inst <i>` to produce a concrete
+`<decoded_inst>` and return an `exec_inst <decoded_inst> <state>`
+term.
+
+`sym1` then applies some lemmas in its simpset, and finally we
+capture the behavior of the instruction in terms of writes made to
+the machine state (denoted by function `w`).
+-/
+
 ----------------------------------------------------------------------
 
--- Symbolic Simulation Test 1:
+-- Test 1: We have a 4 instruction program, and we attempt to simulate
+-- all of them.
 
--- In theorems sha512_program_snippet1_sym and
--- sha512_program_snippet2_sym below, we attempt to symbolically
--- simulate programs sha512_program_snippet1 and
--- sha512_program_snippet2 (respectively). Note that the first four
--- instructions in sha512_program_snippet2 are identical to
--- sha512_program_snippet1.
-
--- The `sym1` tactic, defined in ../Tactics/Sym.lean, attempts to
--- symbolically simulate a single instruction -- it is pretty
--- straightforward right now. It unfolds the `run` function once, then
--- unfolds the `stepi` function, reduces
--- `fetch_inst <address> <state>`
--- to a term like:
--- `Std.RBMap.find?  <program> <address>`
--- where `<program>` and `<address>` are expected to be concrete.
--- We hope that `simp` is able to "evaluate" such ground terms during
--- proof so that the above gives us instruction <i> located at
--- `<address>` in `<program>`. Moreover, we also hope that `simp`
--- evaluates `decode_raw_inst <i>` to produce a concrete
--- `<decoded_inst>` and return an `exec_inst <decoded_inst> <state>`
--- term.
---
--- `sym1` then applies some lemmas in its simpset, and finally we
--- capture the behavior of the instruction in terms of writes made to
--- the machine state (denoted by function `w`).
-
--- However, sha512_program_snippet1_sym is proved by following this
--- strategy, but sha512_program_snippet2_sym times out because `simp
--- ground` times out.
-
-def sha512_program_snippet1 : program :=
+def sha512_program_test_1 : program :=
   def_program
    #[(0x126538#64 , 0xcec08230#32),      --  sha512su0       v16.2d, v17.2d
      (0x12653c#64 , 0x6e154287#32),      --  ext     v7.16b, v20.16b, v21.16b, #8
@@ -52,18 +47,22 @@ def sha512_program_snippet1 : program :=
      (0x126544#64 , 0xce678af0#32)       --  sha512su1       v16.2d, v23.2d, v7.2d
      ]
 
--- WORKING
 -- set_option profiler true in
-theorem sha512_program_snippet1_sym (s : ArmState)
+theorem sha512_program_test_1_sym (s : ArmState)
   (h_pc : read_pc s = 0x126538#64)
-  (h_program : s.program = sha512_program_snippet1.find?)
+  (h_program : s.program = sha512_program_test_1.find?)
   (h_s_ok : read_err s = StateError.None)
   (h_s' : s' = run 4 s) :
   read_err s' = StateError.None := by
   iterate 4 (sym1 [h_program])
   done
 
-def sha512_program_snippet2 : program :=
+----------------------------------------------------------------------
+
+-- Test 2: We have a 5 instruction program, and we attempt to simulate
+-- only 4 of them.
+
+def sha512_program_test_2 : program :=
   def_program
    #[(0x126538#64 , 0xcec08230#32),      --  sha512su0       v16.2d, v17.2d
      (0x12653c#64 , 0x6e154287#32),      --  ext     v7.16b, v20.16b, v21.16b, #8
@@ -74,9 +73,9 @@ def sha512_program_snippet2 : program :=
      ]
 
 -- set_option profiler true in
-theorem sha512_program_snippet2_sym (s : ArmState)
+theorem sha512_program_test_2_sym (s : ArmState)
   (h_pc : read_pc s = 0x126538#64)
-  (h_program : s.program = sha512_program_snippet2.find?)
+  (h_program : s.program = sha512_program_test_2.find?)
   (h_s_ok : read_err s = StateError.None)
   (h_s' : s' = run 4 s) :
   read_err s' = StateError.None := by
@@ -84,73 +83,9 @@ theorem sha512_program_snippet2_sym (s : ArmState)
 
 ----------------------------------------------------------------------
 
--- Symbolic Simulation Test 2: sha512_block_armv8_one_iteration ---
--- this is the same symbolic simulation test for a longer program. I
--- leave it here as a test to stress-test `simp`.
+-- Test 3:
 
--- @[simp]
--- theorem fetch_inst_at_0x1264c0
---   (h_program : s.program = sha512_program_map.find?) :
---   fetch_inst 0x1264c0#64 s = 0xa9bf7bfd#32 := by
---   rw [fetch_inst_from_rbmap_program h_program]
---   native_decide
--- @[simp]
--- theorem fetch_inst_at_0x1264c4
---   (h_program : s.program = sha512_program_map.find?) :
---   fetch_inst 0x1264c4#64 s = 0x910003fd#32 := by
---   rw [fetch_inst_from_rbmap_program h_program]
---   native_decide
--- @[simp]
--- theorem fetch_inst_at_0x1264c8
---   (h_program : s.program = sha512_program_map.find?) :
---   fetch_inst 0x1264c8#64 s = 0x4cdf2030#32 := by
---   rw [fetch_inst_from_rbmap_program h_program]
---   native_decide
--- @[simp]
--- theorem fetch_inst_at_0x1264cc
---   (h_program : s.program = sha512_program_map.find?) :
---   fetch_inst 0x1264cc#64 s = 0x4cdf2034#32 := by
---   rw [fetch_inst_from_rbmap_program h_program]
---   native_decide
--- @[simp]
--- theorem fetch_inst_at_0x1264d0
---   (h_program : s.program = sha512_program_map.find?) :
---   fetch_inst 0x1264d0#64 s = 0x4c402c00#32 := by
---   rw [fetch_inst_from_rbmap_program h_program]
---   native_decide
--- @[simp]
--- theorem fetch_inst_at_0x1264d4
---   (h_program : s.program = sha512_program_map.find?) :
---   fetch_inst 0x1264d4#64 s = 0xd0000463#32 := by
---   rw [fetch_inst_from_rbmap_program h_program]
---   native_decide
-
--- set_option profiler true in
-theorem sha512_block_armv8_one_iteration (s : ArmState)
-  (h_s_ok : read_err s = StateError.None)
-  (h_sp_aligned : CheckSPAlignment s = true)
-  (h_pc : read_pc s = 0x1264c0#64)
-  (h_program : s.program = sha512_program_map.find?)
-  (h_s' : s' = run 32 s) :
-  read_err s' = StateError.None := by  
-  -- sym1 [h_program];
-    -- sym1 [h_program];
-    -- save;
-    -- -- sym1 [h_program];
-    -- (try simp_all); unfold run; simp_all [stepi];
-    -- (try rw [fetch_inst_from_rbmap_program $h_program]);
-    -- conv =>
-    --   pattern fetch_inst _ _
-    --   simp (config := { ground := true }) only
-    --   -- tactic => simp_all! [fetch_inst_at_0x1264c4, -run]
-    --   apply fetch_inst_at_0x1264c4 h_program
-    -- simp (config := { ground := true }) only;
-    -- simp [exec_inst];
-    -- try (simp (config := { ground := true }) only);
-    -- (try simp_all);
-    sorry
-
-def sha512_program_snippet3 : program :=
+def sha512_program_test_3 : program :=
   def_program
   #[(0x1264c0#64 , 0xa9bf7bfd#32),      --  stp     x29, x30, [sp, #-16]!
     (0x1264c4#64 , 0x910003fd#32),      --  mov     x29, sp
@@ -158,14 +93,52 @@ def sha512_program_snippet3 : program :=
     (0x1264cc#64 , 0x4cdf2034#32)       --  ld1     {v20.16b-v23.16b}, [x1], #64
   ]
 
-theorem sha512_block_armv8_test3 (s : ArmState)
+theorem sha512_block_armv8_test_3_sym (s : ArmState)
   (h_s_ok : read_err s = StateError.None)
   (h_sp_aligned : CheckSPAlignment s = true)
   (h_pc : read_pc s = 0x1264c0#64)
-  (h_program : s.program = sha512_program_snippet3.find?)
+  (h_program : s.program = sha512_program_test_3.find?)
   (h_s' : s' = run 4 s) :
   read_err s' = StateError.None := by
-  -- iterate 4 (sym1 [h_program])
+  -- Symbolically simulate one instruction.
+  (sym1 [h_program])
+  --
+  -- (FIXME) At this point, I get an `if` term as the second argument
+  -- of `run`. The if's condition consists of ground terms, and I
+  -- hoped the `if` would be "evaluated away" to the true branch.
+  -- However, the simp/ground below fails with a nested error:
+  -- "maximum recursion depth has been reached".
+  --
+  -- (Aside: I also wish simp/ground didn't leave such a verbose
+  -- output. Anything I can do to fix it?)
+  --
+  -- simp (config := {ground := true}) only
+  --
+  -- (WOKRAROUND) I manually do a split here.
+  split
+  · rename_i h; simp (config := {ground := true}) at h
+  · unfold run
+    simp [stepi, *]
+    rw [fetch_inst_from_rbmap_program h_program]
+    -- (FIXME) Here I run into a similar situation, where we are
+    -- matching on Std.RBMap.find? with ground terms and simp/ground
+    -- fails.
+    sorry
+
+----------------------------------------------------------------------
+
+-- Test 4: sha512_block_armv8_test_4_sym --- this is a symbolic
+-- simulation test for the AWS-LC production SHA512 code (the program
+-- we'd like to verify).
+
+-- set_option profiler true in
+theorem sha512_block_armv8_test_4_sym (s : ArmState)
+  (h_s_ok : read_err s = StateError.None)
+  (h_sp_aligned : CheckSPAlignment s = true)
+  (h_pc : read_pc s = 0x1264c0#64)
+  (h_program : s.program = sha512_program_map.find?)
+  (h_s' : s' = run 32 s) :
+  read_err s' = StateError.None := by
   sorry
 
 end SHA512_proof
