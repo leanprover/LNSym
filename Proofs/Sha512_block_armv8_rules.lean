31df4e5228b6268b0e72cc2bddbd3befbd06124d
Shilpi Goel <shigoel@gmail.com>
2024-06-06 12:06:59 -0500

    Use bv_decide to prove 7 SHA512 rewrite rules; 2 remain

--- Proofs/Sha512_block_armv8_rules.lean
+++ Proofs/Sha512_block_armv8_rules.lean
@@ -5,7 +5,7 @@ Author(s): Shilpi Goel
 -/
 import Arm.Insts.DPSFP.Insts
 import Specs.SHA512
--- import Auto
+import LeanSAT
 
 section sha512_block_armv8_rules
 
@@ -14,16 +14,6 @@ open sha512_helpers
 open DPSFP
 open SHA2
 
--- set_option auto.smt true
--- set_option auto.smt.trust true
--- set_option auto.smt.timeout 20 -- seconds
--- set_option auto.smt.save true
--- -- set_option trace.auto.smt.printCommands true
--- set_option trace.auto.smt.result true -- Print the SMT solver's output
--- set_option trace.auto.smt.model true  -- Print the counterexample, if any
--- set_option trace.auto.smt.proof false -- Do not print the proof.
-
--- set_option auto.smt.savepath "/tmp/sha512_message_schedule_rule.smt2" in
 theorem sha512_message_schedule_rule (a b c d : BitVec 128) :
   sha512su1 a b (sha512su0 c d) =
   let a1 := extractLsb 127 64 a
@@ -35,9 +25,9 @@ theorem sha512_message_schedule_rule (a b c d : BitVec 128) :
   let d0 := extractLsb  63  0 d
   message_schedule_word_aux a1 b1 c0 d1 ++
   message_schedule_word_aux a0 b0 d1 d0 := by
-  sorry -- auto d[sha512su1, sha512su0,  message_schedule_word_aux]
+  simp [sha512su1, sha512su0,  message_schedule_word_aux]
+  bv_decide
 
--- set_option auto.smt.savepath "/tmp/sha512h2_rule.smt2" in
 theorem sha512h2_rule (a b c : BitVec 128) :
   sha512h2 a b c =
   let a0 := extractLsb  63  0  a
@@ -47,7 +37,8 @@ theorem sha512h2_rule (a b c : BitVec 128) :
   let c1 := extractLsb 127 64  c
   ((compression_update_t2 b0 a0 b1) + c1) ++
   ((compression_update_t2 ((compression_update_t2 b0 a0 b1) + c1) b0 b1) + c0) := by
-  sorry -- auto u[maj, compression_update_t2, sha512h2]
+  simp [maj, compression_update_t2, sha512h2, sigma_big_0, ror]
+  bv_decide
 
 -- sha512h2 q3, q1, v0.2d: 0xce608423#32
 -- theorem sha512h2_instruction_rewrite
@@ -67,7 +58,6 @@ theorem sha512h2_rule (a b c : BitVec 128) :
 --           simp (config := { ground := true })
 --           simp [sha512h2_rule]
 
--- set_option auto.smt.savepath "/tmp/sha512h_rule_1.smt2" in
 theorem sha512h_rule_1 (a b c d e : BitVec 128) :
   let elements := 2
   let esize := 64
@@ -86,6 +76,12 @@ theorem sha512h_rule_1 (a b c d e : BitVec 128) :
   let hi64_spec := compression_update_t1 b1 a0 a1 c1 d1 e1
   let lo64_spec := compression_update_t1 (b0 + hi64_spec) b1 a0 c0 d0 e0
   sha512h a b outer_sum = hi64_spec ++ lo64_spec := by
+  simp_all! only [Nat.sub_zero];
+  repeat (unfold binary_vector_op_aux elem_set elem_get; simp)
+  unfold BitVec.partInstall
+  unfold sha512h compression_update_t1 sigma_big_1 ch allOnes ror
+  simp only [Nat.reduceAdd, Nat.reduceSub, Nat.sub_zero, Nat.reducePow]
+  -- bv_decide
   sorry
 /-
   simp_all! only [Nat.sub_zero];
@@ -95,45 +91,40 @@ theorem sha512h_rule_1 (a b c d e : BitVec 128) :
   auto
 -/
 
--- (FIXME) Prove without auto.
--- set_option auto.smt.savepath "/tmp/rev_elems_of_rev_elems_64_8.smt2" in
+-- (FIXME) Generalize to arbitrary-length bitvecs.
 theorem rev_elems_of_rev_elems_64_8 (x : BitVec 64) :
   rev_elems 64 8 (rev_elems 64 8 x h₀ h₁) h₀ h₁ = x := by
   repeat (unfold rev_elems; (simp (config := {ground := true, decide := true})))
   simp_arith at h₀
   simp_arith at h₁
-  sorry -- auto
+  bv_decide
 
--- (FIXME) Prove without auto.
--- set_option auto.smt.savepath "/tmp/concat_of_rsh_is_msb_128.smt2" in
+-- (FIXME) Generalize to arbitrary-length bitvecs.
 theorem concat_of_rsh_is_msb_128 (x y : BitVec 64) :
   (x ++ y) >>> 64 = BitVec.zeroExtend 128 x := by
-  sorry -- auto
+  bv_decide
 
--- (FIXME) Prove without auto.
--- set_option auto.smt.savepath "/tmp/truncate_of_concat_is_lsb_64.smt2" in
+-- (FIXME) Generalize to arbitrary-length bitvecs.
 theorem truncate_of_concat_is_lsb_64 (x y : BitVec 64) :
   BitVec.zeroExtend 64 (x ++ y) = y := by
-  sorry -- auto
+  bv_decide
 
--- (FIXME) Prove without auto.
--- set_option auto.smt.savepath "/tmp/zeroextend_bigger_smaller_64.smt2" in
+-- (FIXME) Generalize to arbitrary-length bitvecs.
 theorem zeroextend_bigger_smaller_64 (x : BitVec 64) :
   BitVec.zeroExtend 64 (BitVec.zeroExtend 128 x) =
   BitVec.zeroExtend 64 x := by
-  sorry -- auto
+  bv_decide
 
 theorem zeroextend_id_64 (x : BitVec 64) :
   BitVec.zeroExtend 64 x = x := by
-  simp
+  simp only [zeroExtend_eq]
 
--- (FIXME) Prove without auto.
--- set_option auto.smt.savepath "/tmp/rsh_concat_identity_128.smt2" in
+-- (FIXME) Generalize to arbitrary-length bitvecs.
 theorem rsh_concat_identity_128 (x : BitVec 128) :
   zeroExtend 64 (x >>> 64) ++ zeroExtend 64 x = x := by
-  sorry
+  bv_decide
 
--- (FIXME) Prove without auto.
+-- (FIXME) Generalize to arbitrary-length bitvecs.
 theorem rev_vector_of_rev_vector_128_64_8 (x : BitVec 128) :
   rev_vector 128 64 8
     (rev_vector 128 64 8 x h₀ h₁ h₂ h₃ h₄) h₀ h₁ h₂ h₃ h₄ = x := by
@@ -147,7 +138,6 @@ theorem rev_vector_of_rev_vector_128_64_8 (x : BitVec 128) :
       rsh_concat_identity_128]
   done
 
--- set_option auto.smt.savepath "/tmp/sha512h_rule_2.smt2" in
 theorem sha512h_rule_2 (a b c d e : BitVec 128) :
   let a0 := extractLsb 63 0   a
   let a1 := extractLsb 127 64 a
@@ -168,7 +158,9 @@ theorem sha512h_rule_2 (a b c d e : BitVec 128) :
   hi64_spec ++ lo64_spec := by
   repeat (unfold binary_vector_op_aux; simp)
   repeat (unfold BitVec.partInstall; simp)
-  unfold sha512h compression_update_t1 sigma_big_1 ch
-  sorry -- auto
+  unfold sha512h compression_update_t1 sigma_big_1 ch ror elem_set elem_get partInstall
+  simp
+  -- bv_decide
+  sorry
 
 end sha512_block_armv8_rules
