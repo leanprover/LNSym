leanprover/LNSym

commit 453e343b5eb585947ba8d72a00570f7e65b8788e

chore: Make ArmState fields private to ensure that every field access is written in terms of r/w and read/write_mem_bytes (#51)
This cleans up our definitions, and forces us to correctly the `read_*`
and `write_*` methods. This is important to allow us to reduce all
(non)interference reasoning into the `r/w` functions.

---------

Co-authored-by: Shilpi Goel <shigoel@gmail.com>

Arm/Cosim.lean
@@ -73,7 +73,7 @@ def init_flags (flags : BitVec 4) (s : ArmState) : ArmState := Id.run do
 /-- Initialize an ArmState for cosimulation from a given regState. -/
 def regState_to_armState (r : regState) : ArmState :=
   let s := init_gprs r.gpr (init_flags r.nzcv (init_sfps r.sfp init_cosim_state))
-  let s := { s with program := def_program [(0x0#64, r.inst)] }
+  let s := set_program s (def_program [(0x0#64, r.inst)])
   s
 
 def bitvec_to_hex (x : BitVec n) : String :=
@@ -128,7 +128,7 @@ bitvector values.-/
 def gpr_list (s : ArmState) : List (BitVec 64) := Id.run do
   let mut acc := []
   for i in [0:31] do
-    acc :=  acc ++ [(s.gpr (BitVec.ofNat 5 i))]
+    acc :=  acc ++ [read_gpr 64 (BitVec.ofNat 5 i) s]
   pure acc
 
 /-- Get the SIMD/FP registers in an ArmState as a list of bitvector
@@ -136,7 +136,7 @@ values.-/
 def sfp_list (s : ArmState) : List (BitVec 64) := Id.run do
   let mut acc := []
   for i in [0:32] do
-    let reg := s.sfp (BitVec.ofNat 5 i)
+    let reg := read_sfp 64 (BitVec.ofNat 5 i) s
     acc := acc ++ [(extractLsb 63 0 reg), (extractLsb 127 64 reg)]
   pure acc
 

Arm/Decode.lean
@@ -3,7 +3,6 @@ Copyright (c) 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author(s): Shilpi Goel
 -/
-import Arm.Memory
 import Arm.Decode.DPI
 import Arm.Decode.DPR
 import Arm.Decode.BR

Arm/Exec.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author(s): Shilpi Goel
 -/
 import Arm.BitVec
-import Arm.Memory
 import Arm.Decode
 import Arm.Insts
 

Arm/Insts/BR/Compare_branch.lean
@@ -7,7 +7,7 @@ Author(s): Shilpi Goel
 -- CBZ, CBNZ -- 32 and 64-bit variants
 
 import Arm.Decode
-import Arm.Memory
+import Arm.State
 import Arm.Insts.Common
 import Arm.BitVec
 

Arm/Insts/BR/Cond_branch_imm.lean
@@ -7,7 +7,7 @@ Author(s): Yan Peng
 -- B.cond
 
 import Arm.Decode
-import Arm.Memory
+import Arm.State
 import Arm.Insts.Common
 import Arm.BitVec
 

Arm/Insts/BR/Uncond_branch_imm.lean
@@ -7,7 +7,7 @@ Author(s): Shilpi Goel
 -- B, BL
 
 import Arm.Decode
-import Arm.Memory
+import Arm.State
 import Arm.Insts.Common
 import Arm.BitVec
 

Arm/Insts/BR/Uncond_branch_reg.lean
@@ -7,7 +7,7 @@ Author(s): Shilpi Goel
 -- RET
 
 import Arm.Decode
-import Arm.Memory
+import Arm.State
 import Arm.Insts.Common
 import Arm.BitVec
 

Arm/Insts/Common.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author(s): Shilpi Goel, Yan Peng
 -/
 import Arm.BitVec
-import Arm.Memory
+import Arm.State
 
 section Common
 

commit d2b073af396d13f92921fa740992186513577fe5

fix: read the full 128 bits of an SFP register in cosimulation (#61)
### Description:

This fixes a conformance testing error introduced in #51, by reading the
full 128 bits of an SFP register instead of only the lower 64

### Testing:

Conformance testing passed

### License:

By submitting this pull request, I confirm that my contribution is
made under the terms of the Apache 2.0 license.

Arm/Cosim.lean
@@ -136,7 +136,7 @@ values.-/
 def sfp_list (s : ArmState) : List (BitVec 64) := Id.run do
   let mut acc := []
   for i in [0:32] do
-    let reg := read_sfp 64 (BitVec.ofNat 5 i) s
+    let reg := read_sfp 128 (BitVec.ofNat 5 i) s
     acc := acc ++ [(extractLsb 63 0 reg), (extractLsb 127 64 reg)]
   pure acc
 