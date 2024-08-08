0431f074a8529d457c5a784c1663c7c7c054041a
Alex Keizer <alex@keizer.dev>
2024-08-08 12:29:09 -0500

    fix: add SPAlignment assumption to gcm_gmult_v8_program_run_27

--- Proofs/AES-GCM/GCMGmultV8Sym.lean
+++ Proofs/AES-GCM/GCMGmultV8Sym.lean
@@ -12,6 +12,7 @@ theorem gcm_gmult_v8_program_run_27 (s0 sf : ArmState)
     (h_s0_program : s0.program = gcm_gmult_v8_program)
     (h_s0_err : read_err s0 = .None)
     (h_s0_pc : read_pc s0 = gcm_gmult_v8_program.min)
+    (h_s0_sp_aligned : CheckSPAlignment s0)
     (h_run : sf = run gcm_gmult_v8_program.length s0) :
     read_err sf = .None := by
   simp (config := {ground := true}) only [Option.some.injEq] at h_s0_pc h_run
