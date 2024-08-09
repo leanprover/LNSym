54da3bca17a60dbb2cb73c0db9304d8c9b996654
Siddharth Bhat <siddu.druid@gmail.com>
2024-08-09 10:17:59 -0500

    chore: move memory into a semarate Arm/Memory/ subdirectory
    This is motivated by more files that will pop up for
    automation via https://github.com/leanprover/LNSym/pull/56/.
    
    It feels like the memory subsystem is now large enough to warrant its own subfolder.

--- Proofs/Experiments/SHA512MemoryAliasing.lean
+++ Proofs/Experiments/SHA512MemoryAliasing.lean
@@ -4,7 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author(s): Shilpi Goel
 -/
 import Arm.Exec
-import Arm.MemoryProofs
+import Arm.Memory.MemoryProofs
 import Specs.SHA512
 -- import Tactics.Sym
 -- import Proofs.SHA512.Sha512StepLemmas
