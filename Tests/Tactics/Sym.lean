f0ebf33b7a9ae79640d1622f8768ae84695fe94e
Alex Keizer <alex@keizer.dev>
2024-08-12 18:43:19 -0500

    feat: make sym_n search for h_err and h_sp, or introduce new goals if they are not present (#75)
    ### Description:
    
    Expands upon #69 to include context search for `h_err` and `h_sp`.
    These assumptions are not strictly required, since we don't reflect
    information from them,
    so if they are not present we introduce a new goal for them (in contrast
    to the other hypotheses, for which we simply throw an error).
    
    ### Testing:
    
    `make all` succeeds, including conformance testing
    
    ### License:
    
    By submitting this pull request, I confirm that my contribution is
    made under the terms of the Apache 2.0 license.
    
    ---------
    
    Co-authored-by: Siddharth <siddu.druid@gmail.com>

---
+++ Tests/Tactics/Sym.lean
@@ -0,0 +1,17 @@
+import Proofs.«AES-GCM».GCMGmultV8Sym
+
+namespace GCMGmultV8Program
+
+-- Test the behaviour of `sym1_n` when `h_err` and `h_sp` are not present
+/-- warning: declaration uses 'sorry' -/ -- `guard_msgs` silences the warning
+#guard_msgs in example (s0 sf : ArmState)
+    (h_s0_program : s0.program = gcm_gmult_v8_program)
+    (h_s0_pc : read_pc s0 = gcm_gmult_v8_program.min)
+    (h_run : sf = run gcm_gmult_v8_program.length s0) :
+    read_err sf = .None := by
+  simp (config := {ground := true}) only [Option.some.injEq] at h_s0_pc h_run
+  sym1_n 1
+  · sorry
+  · exact (sorry : CheckSPAlignment s0)
+  · exact (sorry : read_err s0 = .None)
+  done
