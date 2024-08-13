bbf39463f2dd88c85c219fca1afcfee6fcf3317c
Alex Keizer <alex@keizer.dev>
2024-08-13 16:07:35 -0500

    ensure tests are actually imported

--- Tests/Tactics/Sym.lean
+++ Tests/Tactics/Sym.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author(s): Alex Keizer
+-/
 import Proofs.«AES-GCM».GCMGmultV8Sym
 
 namespace GCMGmultV8Program
