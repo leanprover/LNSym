afe1cda24eb16657d467a979ba66ecdade4108d8
Siddharth <siddu.druid@gmail.com>
2024-08-13 09:15:31 -0500

    Update Arm/State.lean
    Co-authored-by: Shilpi Goel <shigoel@gmail.com>

--- Arm/State.lean
+++ Arm/State.lean
@@ -669,7 +669,7 @@ section NewMemory
 
 For freedom in experimenting with definitions,
 we define our own version of `read_mem` and `write_mem`,
-called `read_mem'` and `write_mem`'.
+called `read_mem'` and `write_mem'`.
 These operate directly on the memory, rather than the `ArmState`.
 We prove their equivalence to the existing definitions
 (`read_mem_eq_read_mem'`, `write_mem_eq_write_mem'`).
