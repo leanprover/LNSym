e896eaa07ac8d909470c631a303bfa7e725e5342
Alex Keizer <alex@keizer.dev>
2024-08-14 09:56:27 -0500

    fix import for SHA512 test

--- Tests/Tactics/ReduceFetchInst.lean
+++ Tests/Tactics/ReduceFetchInst.lean
@@ -5,7 +5,7 @@ Author(s): Alex Keizer
 -/
 import Tactics.Reflect.FetchAndDecode
 import Tests.«AES-GCM».GCMGmultV8Program
-import Proofs.SHA512.SHA512Program
+import Tests.SHA2.SHA512ProgramTest
 
 /-! ## Tests for `refuceFetchInst` simproc -/
 
@@ -13,10 +13,10 @@ example (h : s.program = GCMGmultV8Program.gcm_gmult_v8_program) :
     fetch_inst 0x7d8800#64 s = (some 1279294481#32) := by
   simp (config := {ground := false}) only [reduceFetchInst]
 
-example (h : s.program = sha512_program) :
-    fetch_inst 0x1264c4#64 s = (some 0x910003fd#32) := by
+example (h : s.program = sha512_program_map) :
+    fetch_inst 0x1264c0#64 s = (some 0xa9bf7bfd#32) := by
   simp (config := {ground := false}) only [reduceFetchInst]
 
-example (h : s.program = sha512_program) :
-    fetch_inst 0x126c94#64 s = (some 0x4c002c00#32) := by
+example (h : s.program = sha512_program_map) :
+    fetch_inst 0x126c98#64 s = (some 0xf84107fd#32) := by
   simp (config := {ground := false}) only [reduceFetchInst]
