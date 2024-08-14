b6afa1e3404a9f5e4f47d171feabc7c5695b23db
Alex Keizer <alex@keizer.dev>
2024-08-14 09:40:16 -0500

    simplify proof generation for `reduceFetchInst?`

--- Tactics/Reflect/FetchAndDecode.lean
+++ Tactics/Reflect/FetchAndDecode.lean
@@ -13,10 +13,16 @@ open Elab.Tactic Elab.Term
 initialize
   Lean.registerTraceClass `Sym.reduceFetchInst
 
+theorem fetch_inst_eq_of_prgram_eq_of_map_find
+    {state : ArmState} {program : Program}
+    {addr : BitVec 64} {inst? : Option (BitVec 32)}
+    (h_program : state.program = program)
+    (h_map : program.find? addr = inst?) :
+    fetch_inst addr state = inst? := by
+  rw [fetch_inst, h_program, h_map]
+
 def reduceFetchInst? (addr : Expr) (s : Expr) :
     MetaM (BitVec 32 × Expr) := do
-  let e := mkApp2 (mkConst ``fetch_inst) addr s
-
   let addr ← reflectBitVecLiteral 64 addr
   let ⟨programHyp, program⟩ ← findProgramHyp s
   let programInfo ← try ProgramInfo.lookupOrGenerate program catch err =>
@@ -30,18 +36,17 @@ def reduceFetchInst? (addr : Expr) (s : Expr) :
   trace[Sym.reduceFetchInst] "{Lean.checkEmoji} reduced to: {rawInst}"
 
   -- Now, construct the proof
-  let eqType ← mkEq e (toExpr (some rawInst))
-  trace[Sym.reduceFetchInst] "⚙️ deriving a proof of:\n\t{eqType}"
-
-  let h_program := mkIdent programHyp.userName
-  let eqProofStx ← `(term| by
-    unfold fetch_inst; rw [$h_program:ident]; decide
-  )
-  let proof ← TermElabM.run' <| do
-    let h ← elabTermEnsuringType eqProofStx eqType
-    synthesizeSyntheticMVarsNoPostponing
-    instantiateMVars h
-
+  let proof := mkAppN (mkConst ``fetch_inst_eq_of_prgram_eq_of_map_find) #[
+    s,
+    mkConst program,
+    toExpr addr,
+    toExpr (some rawInst),
+    programHyp.toExpr,
+    mkApp2 (.const ``Eq.refl [1])
+      (mkApp (.const ``Option [0]) <|
+        mkApp (.const ``BitVec []) (toExpr 32))
+      (toExpr (some rawInst))
+  ]
   trace[Sym.reduceFetchInst] "{Lean.checkEmoji} found a proof:\n\t{proof}"
   return ⟨rawInst, proof⟩
 
