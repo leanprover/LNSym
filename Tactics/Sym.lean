6c222c8ddf379e46d34db693e158aef440caa569
Alex Keizer <alex@keizer.dev>
2024-08-16 15:20:36 -0500

    delete old `sym` tactics

--- Tactics/Sym.lean
+++ Tactics/Sym.lean
@@ -10,14 +10,13 @@ import Tactics.ExecInst
 import Tactics.ChangeHyps
 import Tactics.SymContext
 
-import Lean.Elab
-import Lean.Expr
+import Lean
 
 initialize
   Lean.registerTraceClass `Sym
 
 open BitVec
-open Lean (FVarId TSyntax logWarning)
+open Lean
 open Lean.Elab.Tactic (TacticM evalTactic withMainContext)
 
 /-- A wrapper around `evalTactic` that traces the passed tactic script and
@@ -47,52 +46,7 @@ macro "init_next_step" h_run:ident h_step:ident sn:ident : tactic =>
      clear $h_run:ident; rename_i $h_run:ident
      simp (config := {ground := true}) only at $h_run:ident))
 
-def sym_one (curr_state_number : Nat) : TacticM Unit :=
-  withMainContext do
-    let n_str := toString curr_state_number
-    let n'_str := toString (curr_state_number + 1)
-    let mk_name (s : String) : Lean.Name :=
-      Lean.Name.mkStr Lean.Name.anonymous s
-    -- h_st: prefix of user names of hypotheses about state st
-    let h_st_prefix := Lean.Syntax.mkStrLit ("h_s" ++ n_str)
-    -- h_st_program: name of the hypothesis about the program at state st
-    let h_st_program := Lean.mkIdent (mk_name ("h_s" ++ n_str ++ "_program"))
-    let h_st_pc := Lean.mkIdent (mk_name ("h_s" ++ n_str ++ "_pc"))
-    let h_st_err := Lean.mkIdent (mk_name ("h_s" ++ n_str ++ "_err"))
-    let h_st_sp_aligned := Lean.mkIdent (mk_name ("h_s" ++ n_str ++ "_sp_aligned"))
-    -- st': name of the next state
-    let st' := Lean.mkIdent (mk_name ("s" ++ n'_str))
-    -- h_run: name of the hypothesis with the `run` function
-    let h_run := Lean.mkIdent (mk_name "h_run")
-    -- h_step_n': name of the hypothesis with the `stepi` function
-    let h_step_n' := Lean.mkIdent (mk_name ("h_step_" ++ n'_str))
-    evalTactic (←
-      `(tactic|
-         (init_next_step $h_run:ident $h_step_n':ident $st':ident
-          -- Simulate one instruction
-          fetch_and_decode $h_step_n':ident $h_st_prefix:str
-          -- (try clear $h_step_n:ident)
-          exec_inst $h_step_n':ident $h_st_prefix:str
-          intro_fetch_decode_lemmas $h_step_n':ident $h_st_program:ident $h_st_prefix:str
-          (try clear $h_st_pc:ident $h_st_program:ident $h_st_err:ident $h_st_sp_aligned:ident)
-          -- intro_change_hyps $h_step_n':ident $h_st_prefix:str
-          -- (try clear $h_step_n':ident)
-      )))
-
--- sym_n tactic symbolically simulates n instructions.
-elab "sym_n" n:num : tactic => do
-  for i in List.range n.getNat do
-    sym_one i
-
--- sym_n tactic symbolically simulates n instructions from
--- state number i.
-elab "sym_i_n" i:num n:num : tactic => do
-  for j in List.range n.getNat do
-    sym_one (i.getNat + j)
-
 section stepiTac
-
-open Lean Elab Tactic Expr Meta
 
 /-- Apply the relevant pre-generated stepi lemma to replace a local hypothesis
   `h_step : ?s' = stepi ?s`
@@ -144,29 +98,6 @@ def sym1 (c : SymContext) : TacticM SymContext :=
     )
     return c.next
 
-
-open Lean (Name) in
-/-- `sym1_i_n i n h_program` will symbolically evaluate a program for `n` steps,
-starting from state `i`, where `h_program` is an assumption of the form:
-`s{i}.program = someConcreteProgam`.
-
-The context is assumed to contain hypotheses
-```
-h_s{i}_err : r StateField.ERR s{i} = .None
-h_s{i}_pc  : r StateField.PC  s{i} = $PC
-h_run      : sf = run $STEPS s0
-```
-Where $PC and $STEPS are concrete constants.
-Note that the tactic will search for assumption of *exactly* these names,
-it won't search by def-eq -/
-@[deprecated "Use `sym1_n` instead"]
-elab "sym1_i_n" i:num n:num _program:(ident)? : tactic => do
-  Lean.Elab.Tactic.evalTactic (← `(tactic|
-    simp (config := {failIfUnchanged := false}) only [state_simp_rules] at *
-  ))
-  let mut c := SymContext.default i.getNat
-  for _ in List.range n.getNat do
-    c ← sym1 c
 
 /- used in `sym1_n` tactic -/
 syntax sym_at := "at" ident
