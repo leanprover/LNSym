c7470fa31558c3a31efb7d07ff16ec1509e010c9
Siddharth Bhat <siddu.druid@gmail.com>
2024-08-16 13:51:58 -0500

    chore: bring back example committed by shilpi

--- Proofs/Experiments/MemoryAliasing.lean
+++ Proofs/Experiments/MemoryAliasing.lean
@@ -23,3 +23,24 @@ theorem mem_automation_test
 
 /-- info: 'mem_automation_test' depends on axioms: [propext, Classical.choice, Quot.sound] -/
 #guard_msgs in #print axioms mem_automation_test
+
+theorem mem_automation_test_2
+  (h_n0 : n0 ≠ 0)
+  (h_no_wrap_src_region : mem_legal' src_addr ((n0 <<< 4) - 1))
+  (h_no_wrap_dest_region : mem_legal' dest_addr ((n0 <<< 4) - 1))
+  (h_s0_src_dest_separate :
+    mem_separate' src_addr  ((n0 <<< 4) - 1)
+                  dest_addr ((n0 <<< 4) - 1)) :
+  read_mem_bytes 16 src_addr (write_mem_bytes 16 dest_addr blah s0) =
+  read_mem_bytes 16 src_addr s0 := by
+  sorry
+  -- rw [read_mem_bytes_of_write_mem_bytes_different (by decide) (by decide)]
+  -- rwa [@mem_separate_for_subset_general
+  --           src_addr (src_addr + (n0 <<< 4 - 1))
+  --           dest_addr (dest_addr + (n0 <<< 4 - 1))
+  --           src_addr (src_addr + 15#64)
+  --           dest_addr (dest_addr + 15#64)]
+  -- repeat sorry
+
+/-- info: 'mem_automation_test_2' depends on axioms: [propext, sorryAx, Quot.sound] -/
+#guard_msgs in #print axioms mem_automation_test_2
