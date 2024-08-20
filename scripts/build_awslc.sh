3bd3f8923667702d2e5a1ff406a5927fda02aef6
Yan Peng <yppe@amazon.com>
2024-08-20 21:34:48 +0000

    AWS-LC build on MacOS-14 is currently failing

--- scripts/build_awslc.sh
+++ scripts/build_awslc.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Copyright (c) 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # Released under Apache 2.0 license as described in the file LICENSE.
@@ -9,17 +9,33 @@ set -o xtrace
 
 # Remember where LNSym is
 LNSym_DIR=${PWD}
+export CC=clang; export CXX=clang++
+echo $CC
+echo $CXX
+clang --version
+gcc --version
+
+echo ""
+echo "Environment variables:"
+env
 
 # Install dependencies
-brew install ninja golang
+KERNEL_NAME=$(uname -s)
+if [[ "${KERNEL_NAME}" == "Darwin" ]]; then
+  brew install ninja
+else
+  sudo apt-get update
+  sudo apt-get install -y cmake clang ninja-build
+fi
 
-# Fetching and building AWS-LC
+# Fetching AWS-LC
 git clone https://github.com/aws/aws-lc.git $HOME/aws-lc --depth 1
-cd $HOME/aws-lc; mkdir aws-lc-build; cd aws-lc-build
+cd $HOME/aws-lc
+# Build AWS-LC
+mkdir aws-lc-build; cd aws-lc-build
 cmake -GNinja -DKEEP_ASM_LOCAL_SYMBOLS=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo ../
+# cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo ../
 ninja
 
 # Move crypto_test to LNSym
 cp -f crypto/crypto_test ${LNSym_DIR}/Tests/ELFParser/Data/crypto_test
-
-cd ${LNSym_DIR}
