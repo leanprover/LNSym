10bd2847e0e88a821d8fd17c3da68faca72db61e
Yan Peng <yppe@amazon.com>
2024-08-20 20:37:53 +0000

    Separate AWSLCELFTests from the other Tests

---
+++ AWSLCELFTests.lean
@@ -0,0 +1,8 @@
+/-
+Copyright (c) 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author(s): Shilpi Goel
+-/
+-- This module serves as the root of the `Tests` library.
+-- Import modules here that should be built as part of the library.
+import «Tests».AWSLCELFTests
