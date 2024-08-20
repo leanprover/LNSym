a520b3260090ca10ec86b25f8860047f7175a821
Yan Peng <yppe@amazon.com>
2024-08-20 21:06:43 +0000

    Address @bollu's review

--- scripts/build_awslc.sh
+++ scripts/build_awslc.sh
@@ -4,16 +4,19 @@
 # Released under Apache 2.0 license as described in the file LICENSE.
 # Author(s): Yan Peng
 
+set -e
+set -o xtrace
+
 # Remember where LNSym is
-LNSym_DIR = ${PWD}
+LNSym_DIR=${PWD}
 
 # Install dependencies
-brew install ninja
+brew install ninja golang
 
 # Fetching and building AWS-LC
-git clone https://github.com/aws/aws-lc.git $HOME/aws-lc
+git clone https://github.com/aws/aws-lc.git $HOME/aws-lc --depth 1
 cd $HOME/aws-lc; mkdir aws-lc-build; cd aws-lc-build
-cmake -GNinja -DKEEP_ASM_LOCAL_SYMBOLS=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo ../../aws-lc
+cmake -GNinja -DKEEP_ASM_LOCAL_SYMBOLS=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo ../
 ninja
 
 # Move crypto_test to LNSym
