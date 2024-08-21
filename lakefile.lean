c8ac691994150ba5285916643c883486ffdd3fc3
Siddharth Bhat <siddu.druid@gmail.com>
2024-08-21 00:03:05 -0500

    chore: allow building aws-lc crypto_test via docker images

--- lakefile.lean
+++ lakefile.lean
@@ -24,6 +24,9 @@ lean_lib «Tactics» where
 lean_lib «Doc» where
   -- add library configuration options here
 
+lean_lib «AWSLCELFTests» where
+  -- add library configuration options here
+
 @[default_target]
 lean_exe «lnsym» where
   root := `Main
--- scripts/build_awslc.sh
+++ scripts/build_awslc.sh
@@ -9,33 +9,16 @@ set -o xtrace
 
 # Remember where LNSym is
 LNSym_DIR=${PWD}
-export CC=clang; export CXX=clang++
-echo $CC
-echo $CXX
-clang --version
-gcc --version
-
-echo ""
-echo "Environment variables:"
-env
-
-# Install dependencies
-KERNEL_NAME=$(uname -s)
-if [[ "${KERNEL_NAME}" == "Darwin" ]]; then
-  brew install ninja
-else
-  sudo apt-get update
-  sudo apt-get install -y cmake clang ninja-build
-fi
 
 # Fetching AWS-LC
 git clone https://github.com/aws/aws-lc.git $HOME/aws-lc --depth 1
 cd $HOME/aws-lc
-# Build AWS-LC
-mkdir aws-lc-build; cd aws-lc-build
-cmake -GNinja -DKEEP_ASM_LOCAL_SYMBOLS=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo ../
-# cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo ../
-ninja
+
+docker build -t ubuntu-22.04-aarch:base -f tests/ci/docker_images/linux-aarch/ubuntu-22.04_base/Dockerfile tests/ci/docker_images/dependencies
+docker build -t  ubuntu-22.04-gcc-12x tests/ci/docker_images/linux-aarch/ubuntu-22.04_gcc-12x/
+docker run -v `pwd`:`pwd` -w `pwd` ubuntu-22.04-gcc-12x bash -c 'rm -rf build && mkdir -p build && cd build && cmake ../  -DCMAKE_BUILD_TYPE=RelWithDebInfo -DKEEP_ASM_LOCAL_SYMBOLS=1 && make -j4 crypto_test'
+file build/crypto/crypto_test
 
 # Move crypto_test to LNSym
-cp -f crypto/crypto_test ${LNSym_DIR}/Tests/ELFParser/Data/crypto_test
+cp -f build/crypto/crypto_test ${LNSym_DIR}/Tests/ELFParser/Data/crypto_test
+
