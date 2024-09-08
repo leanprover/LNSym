e23d00509f831fe6818a971242a298d2456fd981
Siddharth <siddu.druid@gmail.com>
2024-09-08 14:54:48 -0500

    feat: enable fuel for CSE (#130)
    In doing so, we also cleanup the logging code, since the process of
    adding fuel involves visiting each call site, which is exactly where we
    want to add logging information!
    
    Supercedes #113, as the fixes from that PR have been merged in. Foo #124
    , #114
    
    ---------
    
    Co-authored-by: Alex Keizer <alex@keizer.dev>
    Co-authored-by: Shilpi Goel <shigoel@gmail.com>

--- Tactics/Attr.lean
+++ Tactics/Attr.lean
@@ -2,10 +2,8 @@ import Lean
 
 open Lean
 initialize
-  registerTraceClass `Tactic.cse.collection -- CSE phase that collects expressions.
-  registerTraceClass `Tactic.cse.summary -- CSE phase that summaries information after collection.
-  registerTraceClass `Tactic.cse.generalize -- CSE phase that attempts reperated generalization.
-
+  -- CSE tactic's non-verbose summary logging.
+  registerTraceClass `Tactic.cse.summary 
   -- enable tracing for `sym_n` tactic and related components
   registerTraceClass `Tactic.sym
 
