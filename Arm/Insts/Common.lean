3be9115cfaebb1057c7a45c4f5b0e68a0716ced4
Yan Peng <yppe@amazon.com>
2024-09-19 22:47:22 +0000

    Remove comment

--- Arm/Insts/Common.lean
+++ Arm/Insts/Common.lean
@@ -683,20 +683,6 @@ def shift_right_common_aux
     shift_right_common_aux (e + 1) info operand operand2 result
   termination_by (info.elements - e)
 
-
--- @[app_unexpander BitVec.ofNat] def unexpandBitVecOfNatToHex : Lean.PrettyPrinter.Unexpander
---   | `($(_) $n:num $i:num) =>
---       let i' := i.getNat
---       let n' := n.getNat
---       let trimmed_hex := -- Remove leading zeroes...
---         String.dropWhile (BitVec.ofNat n' i').toHex
---                          (fun c => c = '0')
---                          -- ... but keep one if the literal is all zeros.
---       let trimmed_hex := if trimmed_hex.isEmpty then "0" else trimmed_hex
---       let bv := Lean.Syntax.mkNumLit s!"0x{trimmed_hex}#{n'}"
---       `($bv:num)
---   | _ => throw ()
-
 -- FIXME: should this be upstreamed?
 theorem shift_le (x : Nat) (shift :Nat) :
   x >>> shift ≤ x := by
