19520da61fcf212e39fea0ed935cd1a135b6d7fe
Siddharth Bhat <siddu.druid@gmail.com>
2024-09-19 10:55:25 -0500

    chore: add skip proof

--- Tactics/SkipProof.lean
+++ Tactics/SkipProof.lean
@@ -2,7 +2,9 @@
 Copyright (c) 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Author(s): Siddharth Bhat
+-/
 
+/-
 Tactic to skip proof checking for interactive use.
 -/
 import Lean
