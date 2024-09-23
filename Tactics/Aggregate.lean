d78683f429a9d274cde2a386fc758156c2918539
Siddharth Bhat <siddu.druid@gmail.com>
2024-09-23 18:28:17 -0500

    chore: update author list

--- Tactics/Aggregate.lean
+++ Tactics/Aggregate.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author(s): Alex Keizer
+Author(s): Alex Keizer, Siddharth Bhat
 -/
 import Lean
 import Tactics.Common
