12228e334269eac639e8195763a73a42b9a25a13
Alex Keizer <alex@keizer.dev>
2024-09-24 14:00:58 -0500

    fix: set maxRecDepth properly
    Apparently setting it to 0 does not disable the check, it just makes the limit 0

--- Proofs/SHA512/SHA512Prelude.lean
+++ Proofs/SHA512/SHA512Prelude.lean
@@ -161,8 +161,7 @@ private theorem add_eq_sub_16 (x : BitVec 64) :
 
 -- (TODO) Modifying this theorem is an exercise in patience because of
 -- user-interactivity delays. Let's report this.
-set_option maxRecDepth 0 in
--- | error: ././././Proofs/SHA512/SHA512Prelude.lean:164:0: linter Lean.Linter.constructorNameAsVariable failed: maximum recursion depth has been reached
+set_option maxRecDepth 8000 in
 set_option linter.all false in
 set_option linter.constructorNameAsVariable false in
 theorem sha512_block_armv8_prelude (s0 sf : ArmState)
@@ -281,6 +280,7 @@ theorem sha512_block_armv8_prelude (s0 sf : ArmState)
      (200000) has been reached...`
     -/
     rfl
+
   done
 
 end SHA512
