ed6483016401bf3fefeeb5799d030717501ef276
Alex Keizer <alex@keizer.dev>
2024-09-25 13:56:10 -0500

    chore: add copyright header

--- Benchmarks.lean
+++ Benchmarks.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author(s): Alex Keizer
+-/
 import Benchmarks.SHA512_150
 import Benchmarks.SHA512_225
 import Benchmarks.SHA512_400
