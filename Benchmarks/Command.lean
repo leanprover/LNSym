a5c4d44bd775b0315320fb3309be6b2559c8d989
Alex Keizer <alex@keizer.dev>
2024-09-26 13:10:04 -0500

    feat: withBenchmark combinator for fine-grained benchmarking

--- Benchmarks/Command.lean
+++ Benchmarks/Command.lean
@@ -7,8 +7,18 @@ import Lean
 
 open Lean Parser.Command Elab.Command
 
+initialize
+  registerTraceClass `Benchmark
+
+#check withHeartbeats
+
 elab "benchmark" id:ident declSig:optDeclSig val:declVal : command => do
-  let stx ← `(command| example $declSig:optDeclSig $val:declVal )
+  logInfo m!"Running {id} benchmark\n"
+
+  let stx ← `(command|
+    set_option trace.benchmark true in
+    example $declSig:optDeclSig $val:declVal
+  )
 
   let n := 5
   let mut runTimes := #[]
@@ -39,12 +49,34 @@ elab "benchmark" id:ident declSig:optDeclSig val:declVal : command => do
 NOTE: even if the actual default value changes at some point in the future,
 this value should *NOT* be updated, to ensure the percentages we've reported
 in previous versions remain comparable. -/
-def defaultMaxHeartbeats : Nat := 200000
+private def defaultMaxHeartbeats : Nat := 200000
+
+private def percentOfDefaultMaxHeartbeats (heartbeats : Nat) : Nat :=
+  heartbeats / (defaultMaxHeartbeats * 10)
 
 open Elab.Tactic in
 elab "logHeartbeats" tac:tactic : tactic => do
   let ((), heartbeats) ← withHeartbeats <|
     evalTactic tac
-  let percent := heartbeats / (defaultMaxHeartbeats * 10)
+  let percent := percentOfDefaultMaxHeartbeats heartbeats
 
   logInfo m!"used {heartbeats / 1000} heartbeats ({percent}% of the default maximum)"
+
+variable {m} [Monad m] [MonadLiftT BaseIO m] [MonadLiftT IO m]
+  [MonadOptions m] [MonadTrace m] [MonadRef m] [AddMessageContext m] in
+/-- `withBenchmark x` is a combinator that will trace the time and heartbeats
+used by `x` to the `benchmark` trace class, in a message like:
+  `{header} took {x}s and {y} heartbeats ({z}% of the default maximum)`
+
+NOTE: the maximum reffered to in the message is `defaultMaxHeartbeats`,
+deliberately *not* the currently confiugred `maxHeartbeats` option, to keep the
+numbers comparable across different values of that option. It's thus entirely
+possible to see more than 100% being reported here. -/
+def withBenchmark (header : String) (x : m α) : m α := do
+  let start ← IO.monoMsNow
+  let (a, heartbeats) ← withHeartbeats x
+  let t := ((← IO.monoMsNow) - start)
+  let percent := percentOfDefaultMaxHeartbeats heartbeats
+  trace[Benchmark] m!"{header} took {t}ms and {heartbeats} heartbeats \
+    ({percent}% of the default maximum)"
+  return a
