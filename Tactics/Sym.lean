e5f62fedae80bb8b8ef99c698e91169dcdc7b3a0
Alex Keizer <alex@keizer.dev>
2024-10-03 14:14:05 -0500

    feat: use `withCurrHeartbeats` to reset the heartbeat usage for each step in `sym_n`.
    This unfortunately does not buy us much, as aggregation for 500 already seems to hit both the recursion limit and heartbeat budget, by itself.

--- Tactics/Sym.lean
+++ Tactics/Sym.lean
@@ -231,6 +231,10 @@ elab "explode_step" h_step:term " at " state:term : tactic => withMainContext do
 Symbolically simulate a single step, according the the symbolic simulation
 context `c`, returning the context for the next step in simulation. -/
 def sym1 (whileTac : TSyntax `tactic) : SymM Unit := do
+  /- `withCurHeartbeats` sets the initial heartbeats to the current heartbeats,
+  effectively resetting our heartbeat budget back to the maximum. -/
+  withCurrHeartbeats <| do
+
   let stateNumber ← getCurrentStateNumber
   withTraceNode m!"(sym1): simulating step {stateNumber}" (tag:="sym1") <|
   withMainContext' do
@@ -394,6 +398,7 @@ elab "sym_n" whileTac?:(sym_while)? n:num s:(sym_at)? : tactic => do
         sym1 whileTac
 
     traceHeartbeats "symbolic simulation total"
+    withCurrHeartbeats <|
     withTraceNode "Post processing" (tag := "postProccessing") <| do
       let c ← getThe SymContext
       -- Check if we can substitute the final state
@@ -425,4 +430,4 @@ elab "sym_n" whileTac?:(sym_while)? n:num s:(sym_at)? : tactic => do
         let goal? ← LNSymSimp (← getMainGoal) c.aggregateSimpCtx c.aggregateSimprocs
         replaceMainGoal goal?.toList
 
-    traceHeartbeats "final usage"
+      traceHeartbeats "aggregation"
