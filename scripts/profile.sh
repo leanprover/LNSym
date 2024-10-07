255400be48c84de195202a52924f6420c57500b9
Alex Keizer <alex@keizer.dev>
2024-10-07 17:30:13 -0500

    profile.sh documentation

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -3,6 +3,23 @@
 # Released under Apache 2.0 license as described in the file LICENSE.
 # Author(s): Alex Keizer
 
+# Helper script to get profiling data
+# USAGE:
+#   ./profile.sh Benchmark/file1.lean Benchmark/file2.lean ...
+#
+# The script will first build the `Benchmarks` target, which builds the
+# dependencies but does not run the benchmarks themselves, and then
+# invokes lean directly on each of the files passed as arguments, with
+# - the `profiler` option set to true
+# - the `trace.profiler.output` option set to
+#     `data/profiles/[timestamp]_[commit hash]/[filename].json`, and
+# - the stdout of the build logged in
+#     `data/profiles/[timestamp]_[commit hash]/[filename].log`
+#
+# NOTE: if you pass a file which is not part of the Benchmarks target, then
+#       you *have* to make sure its dependencies are built before invoking this
+#       script
+
 LAKE=lake
 PROF="$LAKE env lean -Dprofiler=true"
 OUT="data/profiles"
