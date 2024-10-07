8a0e247226c380b9d41078f831e3eb91dfe30fe1
Alex Keizer <alex@keizer.dev>
2024-10-07 17:20:17 -0500

    copyright blurbs

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright (c) 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Released under Apache 2.0 license as described in the file LICENSE.
+# Author(s): Alex Keizer
 
 LAKE=lake
 BENCH="$LAKE env lean -Dweak.benchmark.runs=5"
