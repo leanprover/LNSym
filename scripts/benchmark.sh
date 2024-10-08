b98fae2fad538e05bb1bdc53e2ab7b47024c78e0
Alex Keizer <alex@keizer.dev>
2024-10-08 10:03:00 -0500

    doc for benchmark script

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -3,6 +3,22 @@
 # Released under Apache 2.0 license as described in the file LICENSE.
 # Author(s): Alex Keizer
 
+# Helper script to get benchmarking data
+# USAGE:
+#   ./benchmark.sh Benchmark/file1.lean Benchmark/file2.lean ...
+#
+# The script will first build the `Benchmarks` target, which builds the
+# dependencies but does not run the benchmarks themselves, and then
+# invokes lean directly on each of the files passed as arguments, with
+# - the `benchmark.runs` option set to 5, so that each benchmark is run 5
+#     times
+# - the stdout of the build logged in
+#     `data/benchmark/[timestamp]_[commit hash]/[filename]`
+#
+# NOTE: if you pass a file which is not part of the Benchmarks target, then
+#       you *have* to make sure its dependencies are built before invoking this
+#       script
+
 LAKE=lake
 BENCH="$LAKE env lean -Dweak.benchmark.runs=5"
 OUT="data/benchmarks"
